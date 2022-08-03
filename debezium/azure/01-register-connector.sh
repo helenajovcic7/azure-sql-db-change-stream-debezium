6409ddb72e07ba514c8dc32d330bd4a2529bfc89
helenajovcic7 <102991163+helenajovcic7@users.noreply.github.com>
2022-08-03 11:52:18 +0200

    Update 01-register-connector.sh

--- debezium/azure/01-register-connector.sh
+++ debezium/azure/01-register-connector.sh
@@ -3,14 +3,14 @@
 # Strict mode, fail on any error
 #set -euo pipefail
 
-export RESOURCE_GROUP="dm-debezium-ct"
-export CONTAINER_NAME="dm-debezium-ct"
+export RESOURCE_GROUP="dm-debezium-ct2"
+export CONTAINER_NAME="dm-debezium-ct2"
 
 echo "finding debezium ip"
-export DEBEZIUM_IP=`az container show -g $RESOURCE_GROUP -n $CONTAINER_NAME -o tsv --query "ipAddress.ip"`
+export DEBEZIUM_IP2=`az container show -g $RESOURCE_GROUP -n $CONTAINER_NAME -o tsv --query "ipAddress.ip"`
 
 echo "registering connector"
 curl -i -X POST \
     -H "Accept:application/json" -H  "Content-Type:application/json" \
-    http://${DEBEZIUM_IP}:8083/connectors/ \
+    http://${DEBEZIUM_IP2}:8083/connectors/ \
     -d @../register-sqlserver-eh.json
